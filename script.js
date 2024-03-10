3f0e66b606f7e0e6f6a19d119d7b60bd9b7ffe1a
giantsis <giantsis.chris@gmail.com>
2024-03-10 19:12:25 +0200

    full project

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
-const div = document.querySelector("div");
+let color="black";
+let click=false;
+document.addEventListener("DOMContentLoaded",function(){
+
+    document.querySelector("body").addEventListener("click",function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click){
+                draw.innerHTML="Now you can draw.";
+            }
+            else
+                draw.innerHTML="Now you can't draw.";
+        }
+    })
+
+   let btn_popup=document.querySelector("#popup");
+   btn_popup.addEventListener("click",function(){
+    let size=getSize();
+    createBoard(size);
+   })
+
+});
+
+function createBoard(size){
+    let board=document.querySelector(".board");
+
+    board.style.gridTemplateColumns= `repeat(${size},1fr)`;
+    board.style.gridTemplateRows= `repeat(${size},1fr)`;
+
+    let numDivs= size * size ;
+
+    for(let i=0; i<numDivs; i++){
+        let div = document.createElement("div");
+        div.addEventListener("mouseover",divColor)
+        board.insertAdjacentElement("beforeend",div);
+    }
+}
+function getSize(){
+    let input= prompt("What will be the size of the board?");
+    let message = document.querySelector("#message");
+    if (input == ""){
+        message.innerHTML="Please provide a number."
+    }
+    else if (input<0 || input>100){
+        message.innerHTML="Please provide a valid number between 1-100."
+    }
+    else{
+        message.innerHTML="Now you can play.";
+        return input;
+    }
+}
+
+function divColor(){
+    if(click){
+        if (color=="random")
+            this.style.backgroundColor= `hsl(${Math.random()*360},100%,50%)`
+        else
+            this.style.backgroundColor="black";
+    }    
+}
+
+
+function setColor(choice){
+    color=choice;
+}
+
+function resetBoard(){
+    let divs=document.querySelectorAll("div");
+    divs.forEach((div)=>div.style.backgroundColor="white");
+
+    
+}
